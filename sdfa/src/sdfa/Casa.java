19682b234ac37110bb9660ac64dcb8c335d64b2b
Pablo Fernandez <pablof.a3f.4@gmail.com>
2019-05-24 15:29:12 +0200

    sadfsadf
    asdfasfasfdasdf

---
+++ sdfa/src/sdfa/Casa.java
@@ -0,0 +1,15 @@
+package sdfa;
+
+public class Casa {
+	private String adsfa;
+	private int asfasd;
+	
+	/**
+	 * Esto es de prueba
+	 */
+	public void limpiar()
+	{
+		String limp="adskjaslkdjas";
+		System.out.println(limp);
+	}
+}
